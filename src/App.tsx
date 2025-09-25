b963b249398efb937fd81a0f189055c164457ab1
DVKevin <martins.kevin.kk@gmail.com>
2025-09-25 19:09:00 -0300

    feat:criando layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import { Layout } from './componets/layout'
+
 
 function App() {
 
   return (
     <>
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout/>}></Route>
+          <Route path="/add" /><Route />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
